ccc94cad13aadd13eae55f15c5de8f0c85c35911
Noura <nouraaaa108@gmail.com>
2026-02-24 12:24:31 +0400

    feat(star-rating): add rating state lifting and default rating support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={5} color="blue" onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={2}
+    />
+    <Test />
   </React.StrictMode>,
 );
